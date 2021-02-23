3dbe2a9336097277a66d9e807f4fc8dfeb831234
diref ferid <iiidiakoviii@gmail.com>
2021-02-23 02:22:54 +0200

    +одежда

--- Source.cpp
+++ Source.cpp
@@ -7,10 +7,15 @@
 #include <ctime>
 
 using namespace std;
+SDL_Rect block32;
+#define e_down 0
+#define e_up 32
+#define e_right 64
+#define e_left 96
 
 SDL_Texture* create_textureFromPng(SDL_Renderer* ren, const char *name) {
 	SDL_Surface* peps = IMG_Load(name);
-	SDL_Texture* peps_tex = SDL_CreateTextureFromSurface(ren, peps);
+	SDL_Texture* peps_tex = SDL_CreateTextureFromSurface(ren, peps);//проверка
 	SDL_FreeSurface(peps);
 	return peps_tex;
 }
@@ -22,7 +27,7 @@ private:
 public:
 	bool *lethal;
 	int kol;
-	float* hp, *max_hp;
+	float* hp, *max_hp, *wei;
 	string* names;
 	void init(int k) {
 		kol = k;
@@ -30,17 +35,33 @@ public:
 		hp = new float[kol];
 		max_hp = new float[kol];
 		names = new string[kol];
+		wei = new float[kol];
 	};
+	void init_part(int ind, string name, float h, bool let, float wei) {
+		lethal[ind] = let;
+		max_hp[ind] = h; hp[ind] = h;
+		names[ind] = name;
 
+	};
 };
 
 class humanoid
 {
 private:
 	int i;
+	
 public:
-	SDL_Texture* armor= NULL, *weapon= NULL;
-	int body, head, l_arm, r_arm, l_leg, r_leg;
+	SDL_Texture* armor= NULL, *weapon= NULL, *body = NULL;
+	parts scrap;
+	void init_body(SDL_Texture* new_body) {
+		body = new_body;
+	}
+	void draw(SDL_Renderer* ren, SDL_Rect box, int napr) {
+		SDL_RenderCopy(ren, body, &block32, &box);
+		SDL_RenderCopy(ren, armor, &block32, &box); 
+		
+	}
+
 };
 
 int main(int argc,char *argv[]) {
@@ -65,8 +86,8 @@ int main(int argc,char *argv[]) {
 	SDL_SetRenderDrawColor(ren, 100, 0, 0, 255);
 	SDL_RenderClear(ren);
 
-	SDL_Surface* peps = IMG_Load("grass.png");
-	SDL_Texture * peps_tex = SDL_CreateTextureFromSurface(ren, peps);
+	SDL_Surface *peps = IMG_Load("grass.png");
+	SDL_Texture *peps_tex = SDL_CreateTextureFromSurface(ren, peps);
 	SDL_FreeSurface(peps);
 
 
@@ -82,8 +103,9 @@ int main(int argc,char *argv[]) {
 
 	SDL_Texture *grass = create_textureFromPng(ren,"grass.png");
 	SDL_Texture* black = create_textureFromPng(ren, "blackbody.png");
+	SDL_Texture* robe = create_textureFromPng(ren, "robe_white.png");
 	SDL_Rect grass_block;
-	SDL_Rect block32;
+	
 	block32.w = 32;
 	block32.h = 32;
 	block32.x = 0;
@@ -100,6 +122,17 @@ int main(int argc,char *argv[]) {
 	rect.h = 160;
 	rect.x = window_width/2-rect.w/2;
 	rect.y = window_height/2-rect.h/2;
+
+	humanoid player;
+	player.scrap.init(6);
+	player.scrap.init_part(0, "body", 50, 1,35);
+	player.scrap.init_part(1, "head", 30, 1,6);
+	player.scrap.init_part(2, "l_arm", 30, 0, 8);
+	player.scrap.init_part(3, "r_arm", 30, 0, 8);
+	player.scrap.init_part(4, "l_leg", 40, 0, 10);
+	player.scrap.init_part(5, "r_leg", 40, 0, 10);
+	player.armor = robe;
+	player.init_body(black);
 
 	int tick = 0;
 
@@ -160,20 +193,20 @@ int main(int argc,char *argv[]) {
 		}
 		SDL_Delay(fps);
 
-		if (!(key[SDLK_a] || key[SDLK_s] || key[SDLK_d] || key[SDLK_w])) {
-			if ((window_width / 2 - mouse[0] < 0) && (-window_width / 2 + mouse[0] > abs(window_height / 2 - mouse[1]))) {
-				block32.x = 64;
-			}
-			else if ((window_height / 2 - mouse[1] < 0) && (abs(-window_width / 2 + mouse[0]) < -window_height / 2 + mouse[1])) {
-				block32.x = 0;
-			}
-			else if ((window_width / 2 - mouse[0] > 0) && (window_width / 2 - mouse[0] > abs(-window_height / 2 + mouse[1]))) {
-				block32.x = 96;
-			}
-			else {
-				block32.x = 32;
-			}
+		
+		if ((window_width / 2 - mouse[0] < 0) && (-window_width / 2 + mouse[0] > abs(window_height / 2 - mouse[1]))) {
+			block32.x = 64;
 		}
+		else if ((window_height / 2 - mouse[1] < 0) && (abs(-window_width / 2 + mouse[0]) < -window_height / 2 + mouse[1])) {
+			block32.x = 0;
+		}
+		else if ((window_width / 2 - mouse[0] > 0) && (window_width / 2 - mouse[0] > abs(-window_height / 2 + mouse[1]))) {
+			block32.x = 96;
+		}
+		else {
+			block32.x = 32;
+		}
+		
 		tick++;
 		
 		if (tick % 5 == 0) {
@@ -203,7 +236,7 @@ int main(int argc,char *argv[]) {
 			map.y+=320;
 		}
 		
-		SDL_RenderCopy(ren, black, &block32, &rect);
+		player.draw(ren, rect,block32.x);
 		SDL_RenderPresent(ren);
 	}
 
